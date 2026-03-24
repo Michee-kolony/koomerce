8088e45169ba6cffa226cfdb4ef44bc7ce59b2c3
Michée Kolony <micheekolony71@gmail.com>
2026-03-24 15:32:24 +0100

    ajout datas

--- src/app/client/navbar/navbar.component.ts
+++ src/app/client/navbar/navbar.component.ts
@@ -10,6 +10,33 @@ export class NavbarComponent {
  
   search = false;
 
+  produits = [
+  {
+    nom: "Robe élégante",
+    prix: 25,
+    ancienPrix: 40,
+    image: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b"
+  },
+  {
+    nom: "T-shirt streetwear",
+    prix: 12,
+    ancienPrix: 20,
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+  },
+  {
+    nom: "Sneakers modernes",
+    prix: 35,
+    ancienPrix: 50,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+  },
+  {
+    nom: "Sac tendance",
+    prix: 18,
+    ancienPrix: 30,
+    image: "https://images.unsplash.com/photo-1584917865442-de89df76afd3"
+  }
+];
+
 
 launchsearch(){
   this.search = !this.search
