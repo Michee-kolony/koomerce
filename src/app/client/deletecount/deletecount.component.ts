f3814b11e253ba60b5fa5c68d5872399146809a3
Michée Kolony <micheekolony71@gmail.com>
2026-04-01 20:10:41 +0100

    ajout datas

--- src/app/client/deletecount/deletecount.component.ts
+++ src/app/client/deletecount/deletecount.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeletecountComponent implements OnInit {
 
+  user: any = null;
+  isLoggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
-     window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const userData = localStorage.getItem('user');
+
+    if (userData) {
+      this.user = JSON.parse(userData);
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   }
 
 }
